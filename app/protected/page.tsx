77910c58fe857132f83df7e4cb7840f48820dd7c
sujanthapa01 <sujanthapa4001@gmail.com>
2025-11-18 14:39:03 +0530

    test: added test route

---
+++ app/protected/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import { useAuth } from "@/hooks/check_login_user";
+import Logout from "@/components/logout";
+
+function Page() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <p className="text-gray-300 text-lg animate-pulse">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 px-4">
+      <div className="bg-gray-800 shadow-xl rounded-xl p-8 w-full max-w-md text-center border border-gray-700">
+        {user ? (
+          <>
+            <h1 className="text-2xl font-bold text-white mb-2">
+              Welcome 👋
+            </h1>
+
+            <p className="text-gray-300 mb-6">Logged in as:  
+              <span className="text-blue-400 font-semibold"> {user.email}</span>
+            </p>
+
+            <Logout />
+          </>
+        ) : (
+          <p className="text-red-400 text-lg font-semibold">
+            Login first — This route is protected.
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Page;
