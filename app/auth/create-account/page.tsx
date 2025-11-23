d89c9554ca0204bf54342e4a857ea85ee7aaf537
sujanthapa01 <sujanthapa4001@gmail.com>
2025-11-23 22:48:45 +0530

    feat: added create account page

---
+++ app/auth/create-account/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { auth } from "@/lib/firebase";
+import { useRouter } from "next/navigation";
+// import { useAuth } from "@/hooks/check_login_user";
+import { handleFirebaseError } from "@/errors/firebase_Errros";
+
+import {createUserWithEmailAndPassword} from "firebase/auth";
+
+function Page() {
+  const [email, setemail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [print, setPrint] = useState<string>("");
+
+  const handleCreateAccount = async () => {
+    if (!email || !password) {
+      setPrint("Please fill the email and password");
+      return;
+    }
+
+    try {
+      // Try to login
+      await createUserWithEmailAndPassword(auth, email, password);
+      setPrint("Login successful!");
+    } catch (error: any) {
+       const errorMessage = handleFirebaseError(error.code); // Get error message
+      setPrint(errorMessage);
+    }
+  };
+
+
+
+  const router = useRouter()
+
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <div className="flex flex-col gap-4 items-center justify-center h-44">
+        {print && <p className="text-red-400">{print}</p>}
+
+        <input
+          type="text"
+          placeholder="email"
+          onChange={(e) => setemail(e.target.value)}
+          className="border-white rounded-md border-[1px] px-2 py-1 text-white"
+        />
+
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          className="border-white rounded-md border-[1px] px-2 py-1 text-white"
+        />
+
+        <button
+          onClick={handleCreateAccount}
+          className="bg-green-600 rounded-full text-black w-12 h-12 cursor-pointer hover:scale-110 duration-200 border-2 border-transparent hover:border-white flex items-center justify-center"
+        >
+          {"--->"}
+        </button>
+
+      </div>
+
+        <div onClick={() => {router.replace("/auth")}} className=" mt-24 font-thin italic cursor-pointer hover:text-red-600 duration-200 hover:underline">
+         Already have an account?
+        </div>
+    </div>
+  );
+}
+
+export default Page;
