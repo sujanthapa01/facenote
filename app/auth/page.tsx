c51bbb8be35fa7ac2a58242196a5d94f2884354a
sujanthapa01 <sujanthapa4001@gmail.com>
2025-11-23 22:52:02 +0530

    refactor: saprated the error function

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from "react";
 import { auth } from "@/lib/firebase";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/hooks/check_login_user";
+import {handleFirebaseError} from "@/errors/firebase_Errros"
+import {GoogleProvider} from "@/components/GoogleProvider"
 
 import {
   createUserWithEmailAndPassword,
@@ -26,37 +28,8 @@ function Page() {
       await signInWithEmailAndPassword(auth, email, password);
       setPrint("Login successful!");
     } catch (error: any) {
-      // Email not found => Create new account
-      if (error.code === "auth/user-not-found") {
-        try {
-          await createUserWithEmailAndPassword(auth, email, password);
-          setPrint("Account created successfully!");
-          await signInWithEmailAndPassword(auth, email, password);
-        } catch (createError: any) {
-          handleFirebaseError(createError.code);
-        }
-      } else {
-        handleFirebaseError(error.code);
-      }
-    }
-  };
-
-  const handleFirebaseError = (code: string) => {
-    switch (code) {
-      case "auth/invalid-email":
-        setPrint("Invalid email format.");
-        break;
-      case "auth/wrong-password":
-        setPrint("Incorrect password.");
-        break;
-      case "auth/email-already-in-use":
-        setPrint("This email is already registered.");
-        break;
-      case "auth/weak-password":
-        setPrint("Password must be at least 6 characters.");
-        break;
-      default:
-        setPrint("Something went wrong. Try again.");
+       const errorMessage = handleFirebaseError(error.code); // Get error message
+           setPrint(errorMessage); // Set error message to display
     }
   };
 
@@ -69,7 +42,7 @@ function Page() {
   }, [loading, user, router])
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen flex-col">
       <div className="flex flex-col gap-4 items-center justify-center h-44">
         {print && <p className="text-red-400">{print}</p>}
 
@@ -93,7 +66,19 @@ function Page() {
         >
           {"--->"}
         </button>
+
       </div>
+
+<div className="flex flex-col gap-4 items-center justify-center">
+
+        <div onClick={() => {router.replace("/auth/create-account")}} className=" mt-24 font-thin italic cursor-pointer underline hover:text-blue-600 duration-200 hover:underline">
+          create account
+        </div>
+       <span className="italic font-bold text-xl">or</span>
+        <div>
+          <GoogleProvider/>
+        </div>
+</div>
     </div>
   );
 }
